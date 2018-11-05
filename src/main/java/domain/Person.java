cdcbd40bd7b1872c5e393e24ac4cdcaa91c9dd86
wenjie <wenjie@xiaomi.com>
2018-11-05 13:20:20 +0800

    fix

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,39 @@
+package domain;
+
+/**
+ * Person类
+ * @author admin
+ *
+ */
+public class Person {
+	private String name;
+	private int sex;
+	private String address;
+	private String mail;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+}
